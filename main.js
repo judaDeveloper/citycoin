574048a71f39d9ef166dd8c632186669fed767a3
judaDeveloper <166623460+judaDeveloper@users.noreply.github.com>
2024-10-25 03:37:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,26 +7,90 @@ const firebaseConfig = {
     messagingSenderId: "606775878283",
     appId: "1:606775878283:web:07d3d79d7b186be576aa8e"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+var newcid;
 
+const resized = () => {
+	let width = window.innerWidth;
+	let usrfom = document.getElementById('userform').clientWidth;
+	document.getElementById("pagewdth").innerHTML = 'New Member '+width +'/'+ usrfom;
+}
+const resetform = (id) => {
+	document.querySelector(id).reset();
+	document.querySelector('.photo').value ='';
+};
+function radioclicked(radio, inptid) {
+	let value = null;
+	let x = document.getElementsByName(radio);
+	let z = document.getElementById(inptid);
+	for (var i = 0; i < x.length; i++) {		
+		if (x[i].type == 'radio' && x[i].checked == true) {
+			value = x[i].value;
+		}
+	}
+	z.value = value;
+	z.dispatchEvent(new Event('change'));
+	newUserdata();
+}
+function radioValueChanged(radio, inputid) {
+	let x = document.getElementsByName(radio);
+	let n = document.getElementById(inputid);
+	
+	let y = document.querySelector('.' + radio);
+	let z;
+	for (var i = 0; i < x.length; i++) {
+		if (n.value > 0 && x[i].value == n.value) {
+			x[i].checked = true;
+			z = x[i].parentElement.innerText;
+		} else {
+			x[i].checked = false;
+			z = '';
+		}
+		newUserdata();
+
+	} y.value = z;
+}
+function getRandomNumber(min, max) {
+	var minn = Math.ceil(min);
+	var maxx = Math.floor(max);
+	return (Math.floor(Math.random() * (maxx - minn)) + minn);
+}
+function newUserdata() { 
+	const newdata = {
+		cid: 			newcid,
+		firstname: 		document.getElementById("fisrtname").value,
+		secondname: 	document.getElementById("secondname").value,
+		lastname: 		document.getElementById("lastname").value,
+		birthdate: 		document.getElementById("birthdate").value,
+		email: 			document.getElementById("emailaddress").value,
+		gender: 		document.getElementById("inputgenda").value,
+		marital: 		document.getElementById("inputmarital").value,
+		idnumber: 		document.getElementById("idnumber").value,
+		currenttown:	document.getElementById("currenttown").value,
+		sublocation:	document.getElementById("sublocation").value,
+		homeaddress:	document.getElementById("currentaddress").value,
+		phonenumber:	document.getElementById("phonenumber").value,
+		othernumber:	[],
+		imagelink: 		document.getElementById("inputuploadurl").value,
+		date: 			new Date().toLocaleDateString()
+	};
+	localStorage.setItem('newuserdata', JSON.stringify(newdata));
+}
 const resizeimage = () => {
 	let newwidth = 300;
-	let pic = document.getElementById('inputphto').files[0];
+	let x = document.getElementById('inputphto').files[0];
 	let preview = document.querySelector('.photo');
 	let reader = new FileReader();
-
-	reader.readAsDataURL(pic);
-	reader.name = pic.name;
-	reader.size = pic.size;
+	reader.readAsDataURL(x);
+	reader.name = x.name;
+	reader.size = x.size;
 	reader.onload = function(event) {
 		let img = new Image();
 		img.src = event.target.result;
 		img.name = event.target.name;
 		img.size = event.target.size;
-
 		img.onload = function(el) {
 			let x = document.createElement('canvas');
 			let scaleFactor = newwidth / el.target.width;
@@ -35,20 +99,84 @@ const resizeimage = () => {
 			let ctx = x.getContext('2d');
 			ctx.drawImage(el.target, 0, 0, x.width, x.height);
 			let srcEncoded = ctx.canvas.toDataURL('image/png', 1);
-            preview.style. backgroundImage = `url( ${srcEncoded})`;
-
-			//preview.src = srcEncoded;
-			//uploadimage(srcEncoded);	
+            preview.style. backgroundImage = `url( ${srcEncoded})`;	
+			uploadimage(srcEncoded);
 		};
 	};
 };
+const uploadimage = (srcEncoded) => {
+	let inputurl = document.getElementById("inputuploadurl");
+	let ref = firebase.storage().ref("/UserImages");
 
-function previewimage() {
-    var url = localStorage.getItem('tempImage');
-    if (url) {
-        document.getElementById('photoicon').style. backgroundImage = "url('your-image-url')";
-    }
-}
+	let name = +new Date()+ "00000";
+	ref.child(name).putString(srcEncoded, 'data_url').then((snapshot) => snapshot.ref.getDownloadURL()).then(myurl => {
+		if (myurl !== '') {
+			inputurl.value = myurl;
+			inputurl.classList.add('uploaded');
+			inputurl.dispatchEvent(new Event('change'));
+			document.querySelector('.photo').style. backgroundImage = `url( ${inputurl.value})`;	
+		}
+	});
+};
+
+const saveuser = () => {
+	let data = JSON.parse(localStorage.getItem("newuserdata"));
+	 db.collection("citycoinCloudusers").add(data)
+	.then((docRef) => {
+		alert("You have successfully submited your data for registration process!");
+		Window.close();
+		localStorage.removeItem('newuserdata');
+	})
+	.catch((error) => {
+		alert("Error adding document: ", error);
+	});
+};
+
+const getnewuserId = () => {
+	let array = [];
+	let rnum = getRandomNumber(1000001, 9999999);
+	let ref = db.collection('citycoinCloudusers');
+
+	ref.get().then(snap => {
+		snap.forEach(doc => {
+			array.push(doc.data().cid);
+		});
+		if (array.length > 0) {
+			if (rnum.toString().length == 7 && array.includes(rnum) == false) {
+				newcid = Number(rnum);
+				newUserdata();
+				saveuser();
+
+			} else {
+				getnewuserId();
+			}
+		} else {
+			newcid = 1000001;
+			newUserdata();
+			saveuser();
+		}
+	});
+};
+
+const deleteUnusedId = () => {
+	const docref = db.collection('Citycoin_clients').doc(documentId.toString());
+	docref.get().then((doc) => {
+		if (doc.exists) {
+			if (doc.data().state == 'new') {
+				docref.delete().then(() => {
+					alert("Document successfully deleted!");
+
+				}).catch((error) => {
+					alert("Error removing document: ", error);
+				});
+			}
+		} else {
+			alert("No such document!");
+		}
+	}).catch((error) => {
+		alert("Error getting document:", error);
+	});
+};
 
 
 
